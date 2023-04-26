e3dac581aa2082a73adc23be9ffbc06dcc0a556c
Xamana <gamingtest@mail.ru>
2023-04-27 00:00:21 +0300

    add skeletin and fetch data from mockapi

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ import { Header } from './Components/Header';
 import { Sort } from './Components/Sort';
 import { Categories } from './Components/Categories';
 import { PizzaItem } from './Components/PizzaItem';
-import pizzas from './assets/pizzas.json'
+import { useState, useEffect } from 'react';
+import { Sceleton } from './Components/PizzaItem/Skeleton';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoding, setIsLodaing] = useState(true);
+
+  useEffect(() => {
+    fetch('https://64497c2fb88a78a8f0092e91.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr)
+        setIsLodaing(false)
+      });
+  }, []);
+
   return (
     <div className='wrapper'>
       <Header />
@@ -15,11 +30,15 @@ function App() {
         <div className='container'>
           <div className='content__top'>
             <Categories />
-            <Sort/>
+            <Sort />
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-          {pizzas.map((obj) => <PizzaItem {...obj}/>)}
+            {
+              isLoding 
+                ? [...new Array(6)].map((_, i) => <Sceleton key={i}/>)
+                : items.map((obj) => (<PizzaItem {...obj} />))
+            }
           </div>
         </div>
       </div>
---
+++ src/Components/PizzaItem/Skeleton.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+export const Sceleton = (props) => (
+  <ContentLoader 
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={466}
+    viewBox="0 0 280 466"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <circle cx="134" cy="136" r="125" /> 
+    <rect x="1" y="284" rx="10" ry="10" width="280" height="27" /> 
+    <rect x="0" y="321" rx="10" ry="10" width="280" height="88" /> 
+    <rect x="0" y="439" rx="10" ry="10" width="91" height="27" /> 
+    <rect x="118" y="421" rx="20" ry="20" width="152" height="45" />
+  </ContentLoader>
+)
